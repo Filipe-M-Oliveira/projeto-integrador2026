4671baeffd4e816fbaec9a0bc80886d1b5e432c9
Filipe Mamud Oliveira <filipe_mamud@hotmail.com>
2026-06-05 17:36:02 -0300

    resolvendo tudo

--- cortinas.js
+++ cortinas.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 window.addEventListener("load", () => {
     const overlay = document.getElementById("cortina-camada");
 
@@ -15,23 +13,3 @@ window.addEventListener("load", () => {
         overlay.classList.add("sumir");
     }, 2400);
 });
-
-=======
-
-window.addEventListener("load", () => {
-    const overlay = document.getElementById("cortina-camada");
-
-    if (!overlay) return;
-
-    // começa a abrir a cortina
-    setTimeout(() => {
-        overlay.classList.add("abrir");
-    }, 500);
-
-    // depois que abrir, faz a camada sumir suavemente
-    setTimeout(() => {
-        overlay.classList.add("sumir");
-    }, 2400);
-});
-
->>>>>>> f1c5fd7c0d4ae39f93cb518e9a5dfd7effa7f862
